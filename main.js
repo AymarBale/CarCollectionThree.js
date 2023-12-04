1e5327defe1869b34f5e1ef35c3bf590af839017
nathanael bale <aymarbale@gmail.com>
2023-12-04 02:31:42 -0500

    resozing

--- main.js
+++ main.js
@@ -148,6 +148,10 @@ function updateProgressBar() {
     }
 }
 
-
+window.addEventListener('resize', function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
 init();
